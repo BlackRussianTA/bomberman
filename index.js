3dad2ca4246bcc0d87465c9ab00492a9c98ec0eb
Antoan Elenkov <antoanelenkov@gmail.com>
2015-07-29 19:51:26 +0300

    working bomb

--- index.js
+++ index.js
@@ -425,7 +425,7 @@ window.onload = function () {
             game.player_move('right');
         } else if (ev.keyCode === 40  || ev.keyCode === 83) {
             game.player_move('down');
-        }else if (ev.keyCode === 32) {
+        }else if (ev.keyCode === 32 ) {
             game.put_bomb();
         }
     };
