71a943fd0df4c43b00c4c087f455405b4436ae61
Antoan Elenkov <antoanelenkov@gmail.com>
2015-07-31 02:42:42 +0300

    add presentation

--- Heat.Revenge/UI/Scripts/impress.js
+++ Heat.Revenge/UI/Scripts/impress.js
@@ -1,6 +1,6 @@
 /**
  * impress.js
- **
+ *
  * impress.js is a presentation tool based on the power of CSS3 transforms and transitions
  * in modern browsers and inspired by the idea behind prezi.com.
  *
