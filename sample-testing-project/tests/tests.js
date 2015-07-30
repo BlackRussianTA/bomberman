a80e52e5d69e63f04f8da97bf64cae89da123ea1
V3ronique <veronica.velkova@gmail.com>
2015-07-30 06:07:04 +0300

    Working attempts at Unit testing.

---
+++ sample-testing-project/tests/tests.js
@@ -0,0 +1,35 @@
+before(function () {
+
+    this.game = game;
+    this.module = module;
+    this.helpers = helpers in module;
+
+});
+
+describe('Test for game', function () {
+    it('test 1', function () {
+        chai.assert.equal(5, 5);
+    });
+
+    it('game to exist', function(){
+        chai.assert.typeOf(game, 'function')
+    });
+
+    //it('game to exist', function(){
+      //  chai.assert.typeOf(game, 'undefined')
+    //})
+});
+
+describe('Test for module', function(){
+    it('test for module to exist', function(){
+        chai.assert.typeOf(module, 'object')
+    });
+});
+
+describe('Test for module', function(){
+    it('test for module to exist', function(){
+        chai.assert.typeOf(helpers, 'object')
+    });
+});
+
+
